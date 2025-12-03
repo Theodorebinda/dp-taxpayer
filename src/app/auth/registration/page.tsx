e963d45ca5add170f8ac3796fcf1b9b3d43fb850
Theodoore samba <theodorebinda@gmail.com>
2025-12-03 15:58:45 +0100

    🚀feat: enhance registration page layout with improved mobile responsiveness and styling adjustments. Update main container properties for better overflow handling and visual clarity.

--- src/app/auth/registration/page.tsx
+++ src/app/auth/registration/page.tsx
@@ -39,7 +39,7 @@ export default function DigiPublicSignupForm() {
   const step = steps[currentStep];
 
   return (
-    <main className="w-full relative flex items-center lg:gap-5 h-screen bg-background max-lg:flex-col-reverse">
+    <main className="w-full relative flex items-center lg:gap-5 h-screen bg-background max-lg:flex-col-reverse max-lg:h-auto max-lg:min-h-screen max-lg:overflow-y-auto">
       {/* Header avec logo et theme toggle */}
       <div className="absolute md:left-5 px-2 md:mx-0   top-5  lg:left-30 z-30 flex w-full md:w-90 md:px-5 justify-between items-center gap-8 md:justify-start">
         <Link
@@ -87,8 +87,8 @@ export default function DigiPublicSignupForm() {
       </div>
 
       {/* Contenu principal */}
-      <div className="w-full max-lg:h-full py-5 max-lg:px-10 flex lg:items-center max-lg:items-center items-start justify-center max-lg:justify-center p-5   max-lg:bg-transparent z-10 relative max-lg:overflow-y-auto">
-        <div className="w-full lg:p-10   max-lg:w-full  lg:w-3/4 flex flex-col gap-4 rounded-xl z-10  bg-background/10 dark:bg-app-blue-700  lg:shadow-lg">
+      <div className="w-full max-lg:min-h-screen max-lg:pt-20 max-lg:pb-5 py-5 max-lg:px-4 flex lg:items-center max-lg:items-start items-start justify-center max-lg:justify-center p-5 max-lg:bg-transparent z-10 relative">
+        <div className="w-full lg:p-10 p-5 max-lg:w-full max-lg:max-w-md lg:w-3/4 flex flex-col gap-4 rounded-xl z-10 backdrop-blur-lg bg-background/10 dark:bg-app-blue-700 lg:shadow-lg">
           {/* Mobile: Stepper */}
           <div className="lg:hidden mb-4">
             <MobileStepper
